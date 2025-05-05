3dfc878ab1474de6f8b0158b3c1ee14b0ce19b8a
UnknownUs3r <thodalj@gmail.com>
2025-05-05 12:37:34 +0100

    simple update.sh

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#set -e
+##################################################################################################################################
+#
+#
+#   RUN AT YOUR OWN RISK
+#
+#
+##################################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+
+#end colors
+#tput sgr0
+##################################################################################################################################
+
+
+sudo apt update -y && sudo apt upgrade -y && flatpak update -y 
